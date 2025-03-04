214db1842d090f121a94898f4764e67bedff1edb
Ben Vessely <benvessely@gmail.com>
2025-03-03 18:22:23 -0800

    Ingredient matching service  works, though algorithm isn't great
    The algorithm isn't good for one word search terms, as any database term
    containing that one word gets full confidence. Could change by
    normilizing by length of database element.

--- src/main/java/benv/recipe/service/IngredientMatchService.java
+++ src/main/java/benv/recipe/service/IngredientMatchService.java
@@ -3,6 +3,8 @@ package benv.recipe.service;
 import benv.recipe.model.IngredientMatchModel;
 import benv.recipe.model.IngredientModel;
 import benv.recipe.model.RecipeModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
@@ -11,10 +13,14 @@ import java.util.*;
 
 @Service
 public class IngredientMatchService {
+    int MATCH_COUNT = 5;
+
     private final JdbcTemplate jdbcTemplate;
     private final IngredientParserService ingredientParserService;
-    private Comparator<IngredientMatchModel> scoreComparator =
+    private final Comparator<IngredientMatchModel> scoreComparator =
             Comparator.comparingDouble(IngredientMatchModel::getConfidence);
+    private static final Logger logger = LoggerFactory.getLogger(IngredientMatchService.class);
+
 
     @Autowired
     private IngredientMatchService(JdbcTemplate jdbcTemplate,
@@ -26,17 +32,19 @@ public class IngredientMatchService {
     public Map<String, List<IngredientMatchModel>> fetchMatches(RecipeModel recipe) {
 
         String[] ingredients = recipe.getIngredients().split("\n");
+//        System.out.printf("Array of ingredients split at newlines is %s%n", Arrays.toString(ingredients));
         Map<String, List<IngredientMatchModel>> matchesContainer = new HashMap<>();
 
         for (String ingredientLine : ingredients) {
+            // System.out.printf("ingredientLine is %s%n", ingredientLine);
             IngredientModel searchModel =  ingredientParserService.parse(ingredientLine);
+            logger.info("searchModel is {}", searchModel.toString());
             PriorityQueue<IngredientMatchModel> matchQueue = new PriorityQueue<>(scoreComparator);
 
             singleMatches(searchModel, matchQueue);
 
             List<IngredientMatchModel> matchList = new ArrayList<>(matchQueue);
-
-            matchesContainer.put(searchModel.getIngredient(), matchList);
+            matchesContainer.put(searchModel.getMainIngredient(), matchList);
         }
 
         return matchesContainer;
@@ -45,13 +53,15 @@ public class IngredientMatchService {
     public void singleMatches(IngredientModel searchModel,
                               PriorityQueue<IngredientMatchModel> matchQueue) {
 
-        String searchTerm = searchModel.getIngredient().trim().toLowerCase();
+        String searchTerm = searchModel.getMainIngredient().trim().toLowerCase();
 
         findExactMatches(searchTerm, matchQueue);
 
+        logger.info("Before check that matchQueue.size() < 5, matchQueue is {}", matchQueue);
         if (matchQueue.size() < 5) {
             List<Map<String, Object>> candidates = new ArrayList<>();
-            reduceCandidates(searchTerm, candidates);
+            candidates = reduceCandidates(searchTerm, candidates);
+            logger.info("candidates List is {}", candidates);
             tokenSearch(searchTerm, candidates, matchQueue);
         }
     }
@@ -59,9 +69,11 @@ public class IngredientMatchService {
     private void findExactMatches(String searchTerm, PriorityQueue<IngredientMatchModel> matchQueue) {
         String sql = "SELECT fdc_id, name FROM ingredients WHERE LOWER(name) = ?";
         List<Map<String, Object>> exactMatches = jdbcTemplate.queryForList(sql, searchTerm);
+        logger.info("In findExactMatches");
 
         if (!exactMatches.isEmpty()){
             for (Map<String, Object> exactMatch : exactMatches ) {
+                logger.info("Exact match is {} for searchTerm {}", exactMatch, searchTerm);
                 IngredientMatchModel newMatch = new IngredientMatchModel();
                 // Need to cast here because queryForList() returns a map with values that are simply Objects
                 newMatch.setFdcId((Integer) exactMatch.get("fdc_id"));
@@ -72,8 +84,8 @@ public class IngredientMatchService {
         }
     }
 
-    private void reduceCandidates(String searchTerm,
-                                  List<Map<String, Object>> candidates) {
+    private List<Map<String, Object>> reduceCandidates(String searchTerm,
+                                                       List<Map<String, Object>> candidates) {
         Set<String> qualifiers = new HashSet<>(Arrays.asList(
                 "dried", "fresh", "frozen", "canned", "sliced", "diced", "chopped",
                 "minced", "grated", "whole", "ground", "crushed", "peeled", "raw",
@@ -105,11 +117,12 @@ public class IngredientMatchService {
             }
         }
 
+        logger.info("mainIngredientVariations are {}", mainIngredientVariations);
+
         StringBuilder sql = new StringBuilder("SELECT fdc_id, name FROM ingredients WHERE ");
         List<String> conditions = new ArrayList<>();
         List<String> params = new ArrayList<>();
 
-        // Remember that mainIngredients holds variations now
         for (String mainIngredientVariation : mainIngredientVariations) {
             if (mainIngredientVariation.length() >= 3) {
                 conditions.add("LOWER(name) LIKE ?");
@@ -119,9 +132,7 @@ public class IngredientMatchService {
 
         sql.append(String.join(" OR ", conditions));
 
-        candidates = jdbcTemplate.queryForList(sql.toString(), params.toArray());
-        System.out.printf("For main ingredient variations {}, number of candidates is {}",
-                mainIngredientVariations, candidates.size());
+        return jdbcTemplate.queryForList(sql.toString(), params.toArray());
     }
 
     public void tokenSearch (String searchTerm, List<Map<String,
@@ -133,19 +144,26 @@ public class IngredientMatchService {
                 "sour", "bitter", "spicy", "hot", "cold", "warm", "ripe", "unripe",
                 "stemmed", "cored"
         ));
-
+        logger.info("In tokenSearch, searchTerm is {}", searchTerm);
 
         String[] searchTokens = searchTerm.split("\\s+");
         for (Map<String, Object> dbCandidate : dbCandidates) {
+            logger.info("dbCandidate is {}", dbCandidate);
             Integer fdcId = (Integer) dbCandidate.get("fdc_id");
             String name = (String) dbCandidate.get("name");
 
             String[] dbCandidateTokens = name.split("\\s+");
+
+            for (int i = 0; i < dbCandidateTokens.length ; i++) {
+                dbCandidateTokens[i] = dbCandidateTokens[i].replace(",", "");
+            }
+
             double totalScore = 0;
             // Will build the max_score as we go and use it to normalize at the end
             double maxScore = 0;
 
             for (String searchToken : searchTokens) {
+                logger.info("searchToken is {}", searchToken);
                 double tokenWeight;
                 // Get lower score for matching with a qualifier than part of main ingredient
                 if (qualifiers.contains(searchToken)) {
@@ -169,8 +187,12 @@ public class IngredientMatchService {
                 }
 
                 for (String dbToken : dbCandidateTokens) {
+                    dbToken = dbToken.toLowerCase();
+                    logger.info("dbToken is {} in Levenshtein loop", dbToken);
                     if (searchToken.length() >= 3 && dbToken.length() >= 3) {
                         double similarity = normalizedLevSimilarity(searchToken, dbToken);
+                        logger.info("Similarity score for search token {} and db " +
+                                "token {} is {}", searchToken, dbToken, similarity);
                         bestMatchScore = Math.max(bestMatchScore, similarity);
                     }
                 }
@@ -178,32 +200,33 @@ public class IngredientMatchService {
                 totalScore += bestMatchScore * tokenWeight;
             }
             double normalizedScore = totalScore / maxScore;
+            logger.info("normalizedScore is {}", normalizedScore);
             if (normalizedScore > 0.3) {
                 IngredientMatchModel match = new IngredientMatchModel();
                 match.setFdcId(fdcId);
                 match.setName(name);
                 match.setConfidence(normalizedScore);
 
-                addToLimitedQueue(matchQueue, match, 5);
+                addToLimitedQueue(matchQueue, match);
             }
         }
     }
 
     private void addToLimitedQueue(
             PriorityQueue<IngredientMatchModel> queue,
-            IngredientMatchModel item,
-            int maxSize) {
+            IngredientMatchModel item) {
 
-        if (queue.size() < maxSize) {
+        if (queue.size() < MATCH_COUNT) {
             queue.add(item);
         }
         IngredientMatchModel lowest = queue.peek();
+        IngredientMatchModel deleted = null;
         if (lowest != null && lowest.getConfidence() < item.getConfidence()) {
-            queue.poll();
+            deleted = queue.poll();
             queue.offer(item);
+            logger.info("queue updated, deleted {} and added {}", deleted, item);
         }
     }
-
 
 
     private double normalizedLevSimilarity(String searchToken, String dbToken) {
