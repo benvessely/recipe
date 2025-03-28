b584419e99b96f5b19aed8d315fb494062d99c87
Ben Vessely <benvessely@gmail.com>
2025-03-28 13:25:34 -0700

    Integration test possibly functional but overly complicated
    Refactoring now

--- src/test/java/benv/recipe/ThreeStepIntegrationTest.java
+++ src/test/java/benv/recipe/ThreeStepIntegrationTest.java
@@ -2,6 +2,7 @@ package benv.recipe;
 
 import benv.recipe.model.*;
 import benv.recipe.repository.RecipeRepository;
+import benv.recipe.service.IngredientParserService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,6 +28,9 @@ public class ThreeStepIntegrationTest {
 
     @Autowired
     private RecipeRepository recipeRepository;
+
+    @Autowired
+    private IngredientParserService ingredientParserService;
 
     private RecipeModel testRecipe;
     private Integer recipeId;
@@ -60,13 +64,15 @@ public class ThreeStepIntegrationTest {
 
         // Step 2: Test fetching of portions for chosen matches
 
-        List<Integer> selectedFdcIds = extractSelectedFdcIds(matches);
+        Map<String,Integer> ingredientToIdMap = makeIngredientToIdMap(matches);
+
+        List<Integer> selectedFdcIds = new ArrayList<>(ingredientToIdMap.values());
         assertThat(selectedFdcIds).isNotEmpty();
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromPath("/api/recipes/portions");
         // Add (possibly) multiple query parameters with name `fdcIds`, one for the
         // fdcId of each ingredient we've chosen to get portions of. These are converted
-        // by the controller to a List in java
+        // by the controller to a Java List
         for (Integer fdcId : selectedFdcIds) {
             builder.queryParam("fdcIds", fdcId);
         }
@@ -86,43 +92,71 @@ public class ThreeStepIntegrationTest {
 
         // Step 3: Calculate nutrition
 
-        List<IngredientSelectionModel> selections = createSelections(portions);
+        List<IngredientSelectionModel> selections = createSelections(portions, ingredientToIdMap);
 
     }
 
 
-    private List<IngredientSelectionModel> createSelections(Map<String, List<PortionModel>> portions) {
+    private List<IngredientSelectionModel> createSelections(Map<String, List<PortionModel>> portions,
+                                                            Map<String, Integer> ingredientToIdMap) {
         Pattern WEIGHT_PATTERN =
                 Pattern.compile("(g|gram|grams|oz|ounce|ounces|lb|lbs|pound|pounds|kg|kilogram|kilograms)");
 
         List<IngredientSelectionModel> selections = new ArrayList<>();
 
+        Map<String, IngredientModel> parsedMap = new HashMap<>();
         RecipeModel recipe = recipeRepository.getRecipeById(recipeId);
-        String recipeIngredients = recipe.getIngredients();
 
-
-
-        for (List<PortionModel> portionList : portions.values()) {
-            IngredientSelectionModel selection = new IngredientSelectionModel();
-
-            Matcher matcher = WEIGHT_PATTERN.matcher(unit);
+        String[] ingredients = recipe.getIngredients().split("\n");
+        // The map we are adding to here makes it easy to reference the parsed ingredient
+        // when we are looping through the portions map by ingredient name below
+        for (String ingredientLine : ingredients) {
+            IngredientModel parsedIngredient = ingredientParserService.parse(ingredientLine);
+            parsedMap.put(parsedIngredient.getMainIngredient(), parsedIngredient);
         }
+
+        for (String ingredientName : portions.keySet()) {
+            IngredientSelectionModel selection = new IngredientSelectionModel();
+            IngredientModel parsedIngredient = parsedMap.get(ingredientName);
+
+            Matcher matcher = WEIGHT_PATTERN.matcher(parsedIngredient.getUnit());
+
+            // If the unit for this ingredient is already a weight
+            if (matcher.matches()) {
+                selection.setPortionId(null);
+                Integer fdcId = ingredientToIdMap.get(ingredientName);
+                selection.setFdcId(fdcId);
+                selection.setQuantity(parsedIngredient.getAmount());
+                selection.setUnit(parsedIngredient.getUnit());
+            } else {
+                // Else if ingredient unit is not a weight, choose the first portion size
+                // arbitrarily for testing simplicity
+                PortionModel zeroethPortion = portions.get(ingredientName).get(0);
+                selection.setPortionId(zeroethPortion.getPortionId());
+                selection.setFdcId(zeroethPortion.getFdcId());
+                selection.setQuantity(1.0);
+                selection.setUnit(zeroethPortion.getUnit());
+            }
+            selections.add(selection);
+        }
+
+        return selections;
     }
 
 
     /**
      * Simulates the selection of elements from the matches list by choosing the zeroeth
-     * element, i.e. the element with the highest confidence
+     * element out of the potential matches for each ingredient, i.e. the element with
+     * the highest confidence
      */
-    private List<Integer> extractSelectedFdcIds(Map<String, List<IngredientMatchModel>> matches) {
-        List<Integer> fdcIds = new ArrayList<>();
+    private Map<String, Integer> makeIngredientToIdMap(Map<String, List<IngredientMatchModel>> matches) {
+        Map<String, Integer> ingredientToIdMap = new HashMap<>();
 
-        for (List<IngredientMatchModel> matchList: matches.values()) {
-            if (!matchList.isEmpty()) {
-                fdcIds.add(matchList.get(0).getFdcId());
-            }
+        for (String ingredient : matches.keySet()) {
+            IngredientMatchModel zeroethMatch = matches.get(ingredient).get(0);
+            ingredientToIdMap.put(ingredient, zeroethMatch.getFdcId());
         }
 
-        return fdcIds;
+        return ingredientToIdMap;
     }
 }
